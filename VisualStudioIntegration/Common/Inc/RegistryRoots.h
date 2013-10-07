f66fab331c052f5645632f4089cb663f04ebbcfe
hardcase <hardcaseminator@gmail.com>
2013-10-07 20:34:13 +0400

    Visual Studio 2012 SDK

---
+++ VisualStudioIntegration/Common/Inc/RegistryRoots.h
@@ -0,0 +1,229 @@
+
+#pragma once
+
+#define REGKEY_VISUALSTUDIOROOT_90        "Software\\Microsoft\\VisualStudio\\9.0"
+#define LREGKEY_VISUALSTUDIOROOT_90       L"Software\\Microsoft\\VisualStudio\\9.0"
+
+#define REGKEY_VISUALSTUDIOROOT_100       "Software\\Microsoft\\VisualStudio\\10.0"
+#define LREGKEY_VISUALSTUDIOROOT_100      L"Software\\Microsoft\\VisualStudio\\10.0"
+
+#define REGKEY_VISUALSTUDIOROOT_110       "Software\\Microsoft\\VisualStudio\\11.0"
+#define LREGKEY_VISUALSTUDIOROOT_110      L"Software\\Microsoft\\VisualStudio\\11.0"
+
+#define REGKEY_VISUALSTUDIOROOT_CURRENT   "Software\\Microsoft\\VisualStudio\\11.0"
+#define LREGKEY_VISUALSTUDIOROOT_CURRENT  L"Software\\Microsoft\\VisualStudio\\11.0"
+
+#define REGKEY_RASCAL_CURRENT   "Software\\Microsoft\\Rascal\\11.0"
+#define LREGKEY_RASCAL_CURRENT  L"Software\\Microsoft\\Rascal\\11.0"
+
+#define VISUALSTUDIO_PROGID_VERSION_100        L"10.0"
+#define VISUALSTUDIO_DDE_NAME_10               L"VisualStudio.10.0"
+
+#define VISUALSTUDIO_PROGID_VERSION_110        L"11.0"
+#define VISUALSTUDIO_DDE_NAME_11               L"VisualStudio.11.0"
+
+#define VISUALSTUDIO_PROGID_VERSION_CURRENT    L"11.0"
+#define VISUALSTUDIO_DDE_NAME_CURRENT          L"VisualStudio.11.0"
+
+#define REGKEY_APPENVROOT_NOVERSION       "Software\\Microsoft\\AppEnv"
+#define LREGKEY_APPENVROOT_NOVERSION      L"Software\\Microsoft\\AppEnv"
+
+#define REGKEY_APPENVROOT_90              "Software\\Microsoft\\AppEnv\\9.0\\Apps"
+#define LREGKEY_APPENVROOT_90             L"Software\\Microsoft\\AppEnv\\9.0\\Apps"
+
+#define REGKEY_APPENVROOT_100             "Software\\Microsoft\\AppEnv\\10.0"
+#define LREGKEY_APPENVROOT_100            L"Software\\Microsoft\\AppEnv\\10.0"
+
+#define REGKEY_APPENVROOT_110             "Software\\Microsoft\\AppEnv\\11.0"
+#define LREGKEY_APPENVROOT_110            L"Software\\Microsoft\\AppEnv\\11.0"
+
+#define REGKEY_APPENVROOT_CURRENT         "Software\\Microsoft\\AppEnv\\11.0"
+#define LREGKEY_APPENVROOT_CURRENT        L"Software\\Microsoft\\AppEnv\\11.0"
+
+#define LREGKEY_SQMREGROOT_90             L"Software\\Microsoft\\VSCommon\\9.0\\SQM"
+#define REGKEY_SQMREGROOT_90              "Software\\Microsoft\\VSCommon\\9.0\\SQM"
+
+#define LREGKEY_SQMREGROOT_100            L"Software\\Microsoft\\VSCommon\\10.0\\SQM"
+#define REGKEY_SQMREGROOT_100             "Software\\Microsoft\\VSCommon\\10.0\\SQM"
+
+#define LREGKEY_SQMREGROOT_110            L"Software\\Microsoft\\VSCommon\\11.0\\SQM"
+#define REGKEY_SQMREGROOT_110             "Software\\Microsoft\\VSCommon\\11.0\\SQM"
+
+#define LREGKEY_SQMREGROOT_CURRENT        L"Software\\Microsoft\\VSCommon\\11.0\\SQM"
+#define REGKEY_SQMREGROOT_CURRENT         "Software\\Microsoft\\VSCommon\\11.0\\SQM"
+
+#define LREGKEY_DEXPLOREROOT_90           L"Software\\Microsoft\\MSDN\\9.0"
+#define REGKEY_DEXPLOREROOT_90            "Software\\Microsoft\\MSDN\\9.0"
+
+#define LREGKEY_DEXPLOREROOT_100          L"Software\\Microsoft\\MSDN\\10.0"
+#define REGKEY_DEXPLOREROOT_100           "Software\\Microsoft\\MSDN\\10.0"
+
+#define LREGKEY_DEXPLOREROOT_110          L"Software\\Microsoft\\MSDN\\11.0"
+#define REGKEY_DEXPLOREROOT_110           "Software\\Microsoft\\MSDN\\11.0"
+
+#define LREGKEY_DEXPLOREROOT_CURRENT      L"Software\\Microsoft\\MSDN\\11.0"
+#define REGKEY_DEXPLOREROOT_CURRENT       "Software\\Microsoft\\MSDN\\11.0"
+
+#define REGKEY_VBEXPRESSROOT_90           "Software\\Microsoft\\VBExpress\\9.0"
+#define LREGKEY_VBEXPRESSROOT_90          L"Software\\Microsoft\\VBExpress\\9.0"
+
+#define REGKEY_VBEXPRESSROOT_100          "Software\\Microsoft\\VBExpress\\10.0"
+#define LREGKEY_VBEXPRESSROOT_100         L"Software\\Microsoft\\VBExpress\\10.0"
+
+#define REGKEY_VBEXPRESSROOT_110          "Software\\Microsoft\\VBExpress\\11.0"
+#define LREGKEY_VBEXPRESSROOT_110         L"Software\\Microsoft\\VBExpress\\11.0"
+
+#define REGKEY_VBEXPRESSROOT_CURRENT      "Software\\Microsoft\\VBExpress\\11.0"
+#define LREGKEY_VBEXPRESSROOT_CURRENT     L"Software\\Microsoft\\VBExpress\\11.0"
+
+#define VBEXPRESS_PROGID_VERSION_100        L"10.0"
+#define VBEXPRESS_DDE_NAME_10               L"VBExpress.10.0"
+
+#define VBEXPRESS_PROGID_VERSION_110        L"11.0"
+#define VBEXPRESS_DDE_NAME_11               L"VBExpress.11.0"
+
+#define VBEXPRESS_PROGID_VERSION_CURRENT    L"11.0"
+#define VBEXPRESS_DDE_NAME_CURRENT          L"VBExpress.11.0"
+
+#define REGKEY_VCEXPRESSROOT_90           "Software\\Microsoft\\VCExpress\\9.0"
+#define LREGKEY_VCEXPRESSROOT_90          L"Software\\Microsoft\\VCExpress\\9.0"
+
+#define REGKEY_VCEXPRESSROOT_100          "Software\\Microsoft\\VCExpress\\10.0"
+#define LREGKEY_VCEXPRESSROOT_100         L"Software\\Microsoft\\VCExpress\\10.0"
+
+#define REGKEY_VCEXPRESSROOT_110          "Software\\Microsoft\\VCExpress\\11.0"
+#define LREGKEY_VCEXPRESSROOT_110         L"Software\\Microsoft\\VCExpress\\11.0"
+
+#define REGKEY_VCEXPRESSROOT_CURRENT      "Software\\Microsoft\\VCExpress\\11.0"
+#define LREGKEY_VCEXPRESSROOT_CURRENT     L"Software\\Microsoft\\VCExpress\\11.0"
+
+#define VCEXPRESS_PROGID_VERSION_100        L"10.0"
+#define VCEXPRESS_DDE_NAME_10               L"VCExpress.10.0"
+
+#define VCEXPRESS_PROGID_VERSION_110        L"11.0"
+#define VCEXPRESS_DDE_NAME_11               L"VCExpress.11.0"
+
+#define VCEXPRESS_PROGID_VERSION_CURRENT    L"11.0"
+#define VCEXPRESS_DDE_NAME_CURRENT          L"VCExpress.11.0"
+
+#define REGKEY_VCSEXPRESSROOT_90          "Software\\Microsoft\\VCSExpress\\9.0"
+#define LREGKEY_VCSEXPRESSROOT_90         L"Software\\Microsoft\\VCSExpress\\9.0"
+
+#define REGKEY_VCSEXPRESSROOT_100         "Software\\Microsoft\\VCSExpress\\10.0"
+#define LREGKEY_VCSEXPRESSROOT_100        L"Software\\Microsoft\\VCSExpress\\10.0"
+
+#define REGKEY_VCSEXPRESSROOT_110         "Software\\Microsoft\\VCSExpress\\11.0"
+#define LREGKEY_VCSEXPRESSROOT_110        L"Software\\Microsoft\\VCSExpress\\11.0"
+
+#define REGKEY_VCSEXPRESSROOT_CURRENT     "Software\\Microsoft\\VCSExpress\\11.0"
+#define LREGKEY_VCSEXPRESSROOT_CURRENT    L"Software\\Microsoft\\VCSExpress\\11.0"
+
+#define VCSEXPRESS_PROGID_VERSION_100        L"10.0"
+#define VCSEXPRESS_DDE_NAME_10               L"VCSExpress.10.0"
+
+#define VCSEXPRESS_PROGID_VERSION_110        L"11.0"
+#define VCSEXPRESS_DDE_NAME_11               L"VCSExpress.11.0"
+
+#define VCSEXPRESS_PROGID_VERSION_CURRENT    L"11.0"
+#define VCSEXPRESS_DDE_NAME_CURRENT          L"VCSExpress.11.0"
+
+#define LREGKEY_VSAROOT_90                L"Software\\Microsoft\\VSA\\9.0"
+#define REGKEY_VSAROOT_90                 "Software\\Microsoft\\VSA\\9.0"
+
+#define LREGKEY_VSAROOT_100               L"Software\\Microsoft\\VSA\\10.0"
+#define REGKEY_VSAROOT_100                "Software\\Microsoft\\VSA\\10.0"
+
+#define LREGKEY_VSAROOT_110               L"Software\\Microsoft\\VSA\\11.0"
+#define REGKEY_VSAROOT_110                "Software\\Microsoft\\VSA\\11.0"
+
+#define LREGKEY_VSAROOT_CURRENT           L"Software\\Microsoft\\VSA\\11.0"
+#define REGKEY_VSAROOT_CURRENT            "Software\\Microsoft\\VSA\\11.0"
+
+#define REGKEY_VSTAROOT_90                "Software\\Microsoft\\VSTA\\9.0" 
+#define LREGKEY_VSTAROOT_90               L"Software\\Microsoft\\VSTA\\9.0"
+
+#define REGKEY_VSTAROOT_100               "Software\\Microsoft\\VSTA\\10.0" 
+#define LREGKEY_VSTAROOT_100              L"Software\\Microsoft\\VSTA\\10.0"
+
+#define REGKEY_VSTAROOT_110               "Software\\Microsoft\\VSTA\\11.0" 
+#define LREGKEY_VSTAROOT_110              L"Software\\Microsoft\\VSTA\\11.0"
+
+#define REGKEY_VSTAROOT_CURRENT           "Software\\Microsoft\\VSTA\\11.0" 
+#define LREGKEY_VSTAROOT_CURRENT          L"Software\\Microsoft\\VSTA\\11.0"
+
+#define LREGKEY_VSTAROOT_InVS_90          L"Software\\Microsoft\\VisualStudio\\9.0\\VSTA"
+
+#define LREGKEY_VSTAROOT_InVS_10          L"Software\\Microsoft\\VisualStudio\\10.0\\VSTA"
+
+#define LREGKEY_VSTAROOT_InVS_11          L"Software\\Microsoft\\VisualStudio\\11.0\\VSTA"
+
+#define LREGKEY_VSTAROOT_InVS_CURRENT     L"Software\\Microsoft\\VisualStudio\\11.0\\VSTA"
+
+#define REGKEY_VW8EXPRESSROOT_110         "Software\\Microsoft\\VSWinExpress\\11.0" 
+#define LREGKEY_VW8EXPRESSROOT_110        L"Software\\Microsoft\\VSWinExpress\\11.0"
+
+#define REGKEY_VW8EXPRESSROOT_CURRENT     "Software\\Microsoft\\VSWinExpress\\11.0" 
+#define LREGKEY_VW8EXPRESSROOT_CURRENT    L"Software\\Microsoft\\VSWinExpress\\11.0"
+
+#define VW8EXPRESS_PROGID_VERSION_110        L"11.0"
+#define VW8EXPRESS_DDE_NAME_11               L"VSWinExpress.11.0"
+
+#define VW8EXPRESS_PROGID_VERSION_CURRENT    L"11.0"
+#define VW8EXPRESS_DDE_NAME_CURRENT          L"VSWinExpress.11.0"
+
+#define REGKEY_VWDEXPRESSROOT_90          "Software\\Microsoft\\VWDExpress\\9.0" 
+#define LREGKEY_VWDEXPRESSROOT_90         L"Software\\Microsoft\\VWDExpress\\9.0"
+
+#define REGKEY_VWDEXPRESSROOT_100         "Software\\Microsoft\\VWDExpress\\10.0" 
+#define LREGKEY_VWDEXPRESSROOT_100        L"Software\\Microsoft\\VWDExpress\\10.0"
+
+#define REGKEY_VWDEXPRESSROOT_110         "Software\\Microsoft\\VWDExpress\\11.0" 
+#define LREGKEY_VWDEXPRESSROOT_110        L"Software\\Microsoft\\VWDExpress\\11.0"
+
+#define REGKEY_VWDEXPRESSROOT_CURRENT     "Software\\Microsoft\\VWDExpress\\11.0" 
+#define LREGKEY_VWDEXPRESSROOT_CURRENT    L"Software\\Microsoft\\VWDExpress\\11.0"
+
+#define REGKEY_VSSERVICINGROOT_CURRENT    "Software\\Microsoft\\Devdiv\\VS\\Servicing\\11.0"
+#define LREGKEY_VSSERVICINGROOT_CURRENT   L"Software\\Microsoft\\Devdiv\\VS\\Servicing\\11.0"
+
+#define VWDEXPRESS_PROGID_VERSION_100        L"10.0"
+#define VWDEXPRESS_DDE_NAME_10               L"VWDExpress.10.0"
+
+#define VWDEXPRESS_PROGID_VERSION_110        L"11.0"
+#define VWDEXPRESS_DDE_NAME_11               L"VWDExpress.11.0"
+
+#define VWDEXPRESS_PROGID_VERSION_CURRENT    L"11.0"
+#define VWDEXPRESS_DDE_NAME_CURRENT          L"VWDExpress.11.0"
+
+#define REGKEY_VPDEXPRESSROOT_100         "Software\\Microsoft\\VPDExpress\\10.0"
+#define LREGKEY_VPDEXPRESSROOT_100        L"Software\\Microsoft\\VPDExpress\\10.0"
+
+#define REGKEY_VPDEXPRESSROOT_110         "Software\\Microsoft\\VPDExpress\\11.0"
+#define LREGKEY_VPDEXPRESSROOT_110        L"Software\\Microsoft\\VPDExpress\\11.0"
+
+#define REGKEY_VPDEXPRESSROOT_CURRENT     "Software\\Microsoft\\VPDExpress\\11.0"
+#define LREGKEY_VPDEXPRESSROOT_CURRENT    L"Software\\Microsoft\\VPDExpress\\11.0"
+
+#define VPDEXPRESS_PROGID_VERSION_100        L"10.0"
+#define VPDEXPRESS_DDE_NAME_10               L"VPDExpress.10.0"
+
+#define VPDEXPRESS_PROGID_VERSION_110        L"11.0"
+#define VPDEXPRESS_DDE_NAME_11               L"VPDExpress.11.0"
+
+#define VPDEXPRESS_PROGID_VERSION_CURRENT    L"11.0"
+#define VPDEXPRESS_DDE_NAME_CURRENT          L"VPDExpress.11.0"
+
+#define REGKEY_WDEXPRESSROOT_110         "Software\\Microsoft\\WDExpress\\11.0" 
+#define LREGKEY_WDEXPRESSROOT_110        L"Software\\Microsoft\\WDExpress\\11.0"
+
+#define REGKEY_WDEXPRESSROOT_CURRENT     "Software\\Microsoft\\WDExpress\\11.0" 
+#define LREGKEY_WDEXPRESSROOT_CURRENT    L"Software\\Microsoft\\WDExpress\\11.0"
+
+#define WDEXPRESS_PROGID_VERSION_110        L"11.0"
+#define WDEXPRESS_DDE_NAME_11               L"WDExpress.11.0"
+
+#define WDEXPRESS_PROGID_VERSION_CURRENT    L"11.0"
+#define WDEXPRESS_DDE_NAME_CURRENT          L"WDExpress.11.0"
+
+#define REGKEY_ADDIN_WIZARD               "Software\\Microsoft\\Shared Tools\\AddinWizard11.0"
+#define LREGKEY_ADDIN_WIZARD              L"Software\\Microsoft\\Shared Tools\\AddinWizard11.0"
